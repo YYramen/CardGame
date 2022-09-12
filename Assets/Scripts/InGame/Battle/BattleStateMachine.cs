9ea133bbc6622c7b8a37240e9e3e150f3b0388a2
YUSUKE <yoshidayusuke1124@gmail.com>
2022-09-12 22:00:28 +0900

    [update] BattleStateMachine更新

--- Assets/Scripts/InGame/Battle/BattleStateMachine.cs
+++ Assets/Scripts/InGame/Battle/BattleStateMachine.cs
@@ -24,7 +24,64 @@ public class BattleStateMachine : BattleStateMachineBase<BattleStateMachine>
         }
         public override void OnExitState()
         {
-            
+            Debug.Log("EnterBattle State を出た");
+        }
+    }
+
+    private class PlayerTurn : BattleStateBase<BattleStateMachine>
+    {
+        public PlayerTurn(BattleStateMachine machine) : base(machine)
+        {
+        }
+        public override void OnEnterState()
+        {
+            Debug.Log("PlayerTurn State に入った");
+        }
+        public override void OnUpdate()
+        {
+
+        }
+        public override void OnExitState()
+        {
+            Debug.Log("PlayerTurn State を出た");
+        }
+    }
+
+    private class EnemyTurn : BattleStateBase<BattleStateMachine>
+    {
+        public EnemyTurn(BattleStateMachine machine) : base(machine)
+        {
+        }
+        public override void OnEnterState()
+        {
+            Debug.Log("EnemyTurn State に入った");
+        }
+        public override void OnUpdate()
+        {
+
+        }
+        public override void OnExitState()
+        {
+            Debug.Log("EnemyTurn State を出た");
+        }
+    }
+
+    private class ExitBattle : BattleStateBase<BattleStateMachine>
+    {
+        public ExitBattle(BattleStateMachine machine) : base(machine)
+        {
+        }
+        public override void OnEnterState()
+        {
+            Debug.Log("ExitBattle State に入った");
+        }
+        public override void OnUpdate()
+        {
+
+        }
+        public override void OnExitState()
+        {
+            Debug.Log("ExitBattle State を出た");
         }
     }
 }
